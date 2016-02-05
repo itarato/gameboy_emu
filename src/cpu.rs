ff264f2248ad6f5b500b6275efb457b93bc63632
Peter Arato <it.arato@gmail.com>
2016-02-05 15:22:19 +0800

    Convert bin opcode matching to hex.

--- src/cpu.rs
+++ src/cpu.rs
@@ -76,7 +76,7 @@ impl CPU {
 
         match opcode {
             // CALL n.
-            0b1100_1101 => {
+            0xCD => {
                 let (addr_hi, addr_lo) = u16_to_hi_lo(self.pc);
                 // TODO review, http://www.devrs.com/gb/files/instr.txt does not mention SP adjustment here
                 self.stack_push(addr_hi, bus);
@@ -87,7 +87,7 @@ impl CPU {
             },
 
             // DEC B.
-            0b0000_0101 => {
+            0x05 => {
                 self.b = if self.b == 0 { 0xFF } else { self.b - 1 };
                 self.flag.z_zero = self.b == 0;
                 self.flag.n_substract = true;
@@ -95,7 +95,7 @@ impl CPU {
             },
 
             // INC C.
-            0b000_1100 => {
+            0x0C => {
                 self.c = if self.c == 0xFF { 0 } else { self.c + 1 };
                 // TODO verify if this is a conditional set or always. Now it's always.
                 self.flag.z_zero = self.c == 0;
@@ -104,14 +104,14 @@ impl CPU {
                 self.flag.h_half_carry = self.c >> 3 & 1 == 1;
             },
             // INC HL.
-            0b0010_0011 => {
+            0x23 => {
                 let (hi, lo) = inc_dd(self.h, self.l);
                 self.h = hi;
                 self.l = lo;
             },
 
             // JR NZ,r8.
-            0b0010_0000 => {
+            0x20 => {
                 let addr = self.read_byte(bus);
                 if !self.flag.z_zero {
                     self.pc = ((self.pc as i16) + ((addr as i8) as i16)) as u16;
@@ -119,37 +119,37 @@ impl CPU {
             },
 
             // LD BC,d16.
-            0b0000_0001 => {
+            0x01 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
                 self.c = vlow;
                 self.b = vhigh;
             },
             // LD C,d8.
-            0b0000_1110 => self.c = self.read_byte(bus),
+            0x0E => self.c = self.read_byte(bus),
             // LD DE,d16.
-            0b0001_0001 => {
+            0x11 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
                 self.e = vlow;
                 self.d = vhigh;
             },
             // LD A,(DE).
-            0b0001_1010 => {
+            0x1A => {
                 let addr = (self.d as u16) << 8 | (self.e as u16);
                 self.acc = bus.read_byte(addr as usize);
             }
             // LD HL,d16.
-            0b0010_0001 => {
+            0x21 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
                 self.l = vlow;
                 self.h = vhigh;
             },
             // LD SP,d16.
-            0b0011_0001 => {
+            0x31 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
                 self.sp = (vhigh as u16) << 8 | (vlow as u16);
             },
             // LD (HL-),A.
-            0b0011_0010 => {
+            0x32 => {
                 let mut addr = hi_lo_to_u16(self.h, self.l);
                 bus.write_byte(addr as usize, self.acc);
 
@@ -161,7 +161,7 @@ impl CPU {
                 self.l = (addr & 0xFF) as u8;
             },
             // LD (HL+),A.
-            0b0010_0010 => {
+            0x22 => {
                 let mut addr = hi_lo_to_u16(self.h, self.l);
                 bus.write_byte(addr as usize, self.acc);
 
@@ -173,21 +173,21 @@ impl CPU {
                 self.l = (addr & 0xFF) as u8;
             },
             // LD A,d8.
-            0b0011_1110 => self.acc = self.read_byte(bus),
+            0x3E => self.acc = self.read_byte(bus),
             // LD C,A.
-            0b0100_1111 => self.c = self.acc,
+            0x4F => self.c = self.acc,
             // LD (HL),A.
-            0b0111_0111 => {
+            0x77 => {
                 let addr = ((self.h as u16) << 8) | (self.l as u16);
                 bus.write_byte(addr as usize, self.acc);
             },
             // LDH (n),A.
-            0b1110_0000 => {
+            0xE0 => {
                 let addr = self.read_byte(bus);
                 bus.write_byte((0xFF00 + (addr as u16)) as usize, self.acc);
             },
             // LD (C),A.
-            0b1110_0010 => {
+            0xE2 => {
                 // let offs = self.read_byte(bus);
                 // bus.write_byte((offs + self.c) as usize, self.acc);
                 // There is some contradiciton here.
@@ -197,41 +197,41 @@ impl CPU {
                 bus.write_byte((0xFF00 + (self.c as u16)) as usize, self.acc);
             },
             // LD B,d8.
-            0b0000_0110 => self.b = self.read_byte(bus),
+            0x06 => self.b = self.read_byte(bus),
 
             // POP BC.
-            0b1100_0001 => {
+            0xC1 => {
                 self.c = self.stack_pop(bus);
                 self.b = self.stack_pop(bus);
             },
 
             // Prefix CB.
-            0b1100_1011 => self.exec_prefixed_instruction(opcode, bus),
+            0xCB => self.exec_prefixed_instruction(opcode, bus),
 
             // PUSH BC.
-            0b1100_0101 => {
+            0xC5 => {
                 let (b, c) = (self.b, self.c);
                 self.stack_push(b, bus);
                 self.stack_push(c, bus);
             },
 
             // RLA.
-            0b0001_0111 => rl!(self, acc),
+            0x17 => rl!(self, acc),
 
             // XOR B.
-            0b1010_1000 => self.b ^= self.b,
+            0xA8 => self.b ^= self.b,
             // XOR C.
-            0b1010_1001 => self.c ^= self.c,
+            0xA9 => self.c ^= self.c,
             // XOR D.
-            0b1010_1010 => self.d ^= self.d,
+            0xAA => self.d ^= self.d,
             // XOR E.
-            0b1010_1011 => self.e ^= self.e,
+            0xAB => self.e ^= self.e,
             // XOR H.
-            0b1010_1100 => self.h ^= self.h,
+            0xAC => self.h ^= self.h,
             // XOR L.
-            0b1010_1101 => self.l ^= self.l,
+            0xAD => self.l ^= self.l,
             // XOR A.
-            0b1010_1111 => self.acc ^= self.acc,
+            0xAF => self.acc ^= self.acc,
 
             _ => panic!("Unknown opcode {:#x} ({:#b}) at PC {:#x} ({})", opcode, opcode, self.pc - 1, self.pc - 1),
         };
@@ -241,14 +241,14 @@ impl CPU {
         let real_opcode = self.read_opcode(bus);
         match real_opcode {
             // BIT 7,H.
-            0b0111_1100 => {
+            0x7C => {
                 self.flag.z_zero = self.h >> 7 == 0;
                 self.flag.n_substract = false;
                 self.flag.h_half_carry = true;
             },
 
             // RL C.
-            0b0001_0001 => rl!(self, c),
+            0x11 => rl!(self, c),
 
             _ => panic!("Unknown perfixed [{:#x} ({:#b})] opcode {:#x} ({:#b})", opcode, opcode, real_opcode, real_opcode),
         }
