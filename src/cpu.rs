02fa4b262e9d7f538bf01110c6d1f2210a3a7ff6
Peter Arato <it.arato@gmail.com>
2016-02-05 15:14:24 +0800

    Add a few commands

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,19 @@
 use bus::{Bus};
 
+macro_rules! rl {
+    ($_self:expr, $reg:ident) => (
+        {
+            let new_carry = $_self.$reg >> 7;
+            $_self.$reg = ($_self.$reg << 1) | $_self.flag.c_carry as u8;
+            $_self.$reg = 1;
+            $_self.flag.c_carry = new_carry == 1;
+            $_self.flag.z_zero = $_self.$reg == 0;
+            $_self.flag.n_substract = false;
+            $_self.flag.h_half_carry = false;
+        }
+    )
+}
+
 const STACK_TOP: u16 = 0xFFFE;
 // TOOD verify it's true
 const STACK_BOTTOM: u16 = 0xFF80;
@@ -10,6 +24,12 @@ fn u16_to_hi_lo(dd: u16) -> (u8, u8) {
 
 fn hi_lo_to_u16(hi:u8, lo:u8) -> u16 {
     (hi as u16) << 8 | lo as u16
+}
+
+fn inc_dd(hi: u8, lo: u8) -> (u8, u8) {
+    let mut val: u16 = hi_lo_to_u16(hi, lo);
+    val += 1;
+    u16_to_hi_lo(val)
 }
 
 #[derive(Default, Debug)]
@@ -55,20 +75,54 @@ impl CPU {
         println!("Read opcode {:#x} ({:#b}) at PC {:#x} ({})", opcode, opcode, self.pc - 1, self.pc - 1);
 
         match opcode {
+            // CALL n.
+            0b1100_1101 => {
+                let (addr_hi, addr_lo) = u16_to_hi_lo(self.pc);
+                // TODO review, http://www.devrs.com/gb/files/instr.txt does not mention SP adjustment here
+                self.stack_push(addr_hi, bus);
+                self.stack_push(addr_lo, bus);
+
+                let (vlow, vhigh) = self.read_low_high(bus);
+                self.pc = hi_lo_to_u16(vhigh, vlow);
+            },
+
+            // DEC B.
+            0b0000_0101 => {
+                self.b = if self.b == 0 { 0xFF } else { self.b - 1 };
+                self.flag.z_zero = self.b == 0;
+                self.flag.n_substract = true;
+                self.flag.h_half_carry = self.b >> 3 & 1 == 0;
+            },
+
+            // INC C.
+            0b000_1100 => {
+                self.c = if self.c == 0xFF { 0 } else { self.c + 1 };
+                // TODO verify if this is a conditional set or always. Now it's always.
+                self.flag.z_zero = self.c == 0;
+                self.flag.n_substract = false;
+                // TODO verify if this a definite set or only when bit 3 == 1
+                self.flag.h_half_carry = self.c >> 3 & 1 == 1;
+            },
+            // INC HL.
+            0b0010_0011 => {
+                let (hi, lo) = inc_dd(self.h, self.l);
+                self.h = hi;
+                self.l = lo;
+            },
+
+            // JR NZ,r8.
+            0b0010_0000 => {
+                let addr = self.read_byte(bus);
+                if !self.flag.z_zero {
+                    self.pc = ((self.pc as i16) + ((addr as i8) as i16)) as u16;
+                }
+            },
+
             // LD BC,d16.
             0b0000_0001 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
                 self.c = vlow;
                 self.b = vhigh;
-            },
-            // INC C.
-            0b000_1100 => {
-                self.c = if self.c == 0xFF { 0 } else { self.c + 1 };
-                // TODO verify if this is a conditional set or always. Now it's conditional.
-                self.flag.z_zero |= self.c == 0;
-                self.flag.n_substract = false;
-                // TODO verify if this a definite set or only when bit 3 == 1
-                self.flag.h_half_carry = (self.c >> 3) & 1 == 1;
             },
             // LD C,d8.
             0b0000_1110 => self.c = self.read_byte(bus),
@@ -83,13 +137,6 @@ impl CPU {
                 let addr = (self.d as u16) << 8 | (self.e as u16);
                 self.acc = bus.read_byte(addr as usize);
             }
-            // JR NZ,r8.
-            0b0010_0000 => {
-                let addr = self.read_byte(bus);
-                if !self.flag.z_zero {
-                    self.pc = ((self.pc as i16) + ((addr as i8) as i16)) as u16;
-                }
-            },
             // LD HL,d16.
             0b0010_0001 => {
                 let (vlow, vhigh) = self.read_low_high(bus);
@@ -103,50 +150,36 @@ impl CPU {
             },
             // LD (HL-),A.
             0b0011_0010 => {
-                let mut addr = ((self.h as u16) << 8) | (self.l as u16);
+                let mut addr = hi_lo_to_u16(self.h, self.l);
                 bus.write_byte(addr as usize, self.acc);
 
-                // TODO make it func or macro.
-                if addr == 0 {
-                    panic!("Address reg HL is zero, cannot be decremented");
-                }
-
+                assert!(addr > 0, "Address reg HL is zero, cannot be decremented");
                 addr -= 1;
+
+                // TODO make it a func or macro.
+                self.h = (addr >> 8) as u8;
+                self.l = (addr & 0xFF) as u8;
+            },
+            // LD (HL+),A.
+            0b0010_0010 => {
+                let mut addr = hi_lo_to_u16(self.h, self.l);
+                bus.write_byte(addr as usize, self.acc);
+
+                assert!(addr < 0xFFFF, "Address reg HL is max (0xFFFF), cannot be incremented");
+                addr += 1;
+
                 // TODO make it a func or macro.
                 self.h = (addr >> 8) as u8;
                 self.l = (addr & 0xFF) as u8;
             },
             // LD A,d8.
             0b0011_1110 => self.acc = self.read_byte(bus),
+            // LD C,A.
+            0b0100_1111 => self.c = self.acc,
             // LD (HL),A.
             0b0111_0111 => {
                 let addr = ((self.h as u16) << 8) | (self.l as u16);
                 bus.write_byte(addr as usize, self.acc);
-            },
-            // XOR B.
-            0b1010_1000 => self.b ^= self.b,
-            // XOR C.
-            0b1010_1001 => self.c ^= self.c,
-            // XOR D.
-            0b1010_1010 => self.d ^= self.d,
-            // XOR E.
-            0b1010_1011 => self.e ^= self.e,
-            // XOR H.
-            0b1010_1100 => self.h ^= self.h,
-            // XOR L.
-            0b1010_1101 => self.l ^= self.l,
-            // XOR A.
-            0b1010_1111 => self.acc ^= self.acc,
-            // CB (Prefix).
-            0b1100_1011 => self.exec_prefixed_instruction(opcode, bus),
-            // CALL n.
-            0b1100_1101 => {
-                let (addr_hi, addr_lo) = u16_to_hi_lo(self.pc);
-                self.stack_push(addr_hi, bus);
-                self.stack_push(addr_lo, bus);
-
-                let (vlow, vhigh) = self.read_low_high(bus);
-                self.pc = hi_lo_to_u16(vhigh, vlow);
             },
             // LDH (n),A.
             0b1110_0000 => {
@@ -163,6 +196,43 @@ impl CPU {
                 // says its a 1 byte op with fixed signing. We follow the latter now.
                 bus.write_byte((0xFF00 + (self.c as u16)) as usize, self.acc);
             },
+            // LD B,d8.
+            0b0000_0110 => self.b = self.read_byte(bus),
+
+            // POP BC.
+            0b1100_0001 => {
+                self.c = self.stack_pop(bus);
+                self.b = self.stack_pop(bus);
+            },
+
+            // Prefix CB.
+            0b1100_1011 => self.exec_prefixed_instruction(opcode, bus),
+
+            // PUSH BC.
+            0b1100_0101 => {
+                let (b, c) = (self.b, self.c);
+                self.stack_push(b, bus);
+                self.stack_push(c, bus);
+            },
+
+            // RLA.
+            0b0001_0111 => rl!(self, acc),
+
+            // XOR B.
+            0b1010_1000 => self.b ^= self.b,
+            // XOR C.
+            0b1010_1001 => self.c ^= self.c,
+            // XOR D.
+            0b1010_1010 => self.d ^= self.d,
+            // XOR E.
+            0b1010_1011 => self.e ^= self.e,
+            // XOR H.
+            0b1010_1100 => self.h ^= self.h,
+            // XOR L.
+            0b1010_1101 => self.l ^= self.l,
+            // XOR A.
+            0b1010_1111 => self.acc ^= self.acc,
+
             _ => panic!("Unknown opcode {:#x} ({:#b}) at PC {:#x} ({})", opcode, opcode, self.pc - 1, self.pc - 1),
         };
     }
@@ -176,6 +246,10 @@ impl CPU {
                 self.flag.n_substract = false;
                 self.flag.h_half_carry = true;
             },
+
+            // RL C.
+            0b0001_0001 => rl!(self, c),
+
             _ => panic!("Unknown perfixed [{:#x} ({:#b})] opcode {:#x} ({:#b})", opcode, opcode, real_opcode, real_opcode),
         }
     }
